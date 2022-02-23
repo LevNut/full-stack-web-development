d329dafd9707306747bd44139743648ec8f7da15
Teeranut Sawanyawat <teeranut.s@ku.th>
2022-02-23 14:13:31 +0000

    add Patch endpoint - update text

--- src/routes/todos/[uid].json.ts
+++ src/routes/todos/[uid].json.ts
@@ -1,6 +1,18 @@
-import type { RequestHandler } from "@sveltejs/kit";
-import { api } from "./_api";
+import type { RequestHandler } from "@sveltejs/kit"
+import { api } from "./_api"
 
-export const del: RequestHandler = (request) => {
-  return api(request);
+export const del = (request) => {
+  return api(request)
+}
+
+export const patch = async (request) => {
+  const data = await request.request.formData()
+  console.log(data);
+
+  return api(request, {
+    uid: "",
+    created_at: new Date(),
+    text: data.get("text").toString(),
+    done: false
+  })
 }
