dd564c409f196a264b0459ebb23587dfbf7c09fc
Teeranut Sawanyawat <teeranut.s@ku.th>
2022-02-23 15:35:52 +0000

    Add Prisma & init migration

--- src/routes/todos/index.json.ts
+++ src/routes/todos/index.json.ts
@@ -8,7 +8,6 @@ export const get: RequestHandler = (request) => {
 export const post = async (request) => {
   const data = await request.request.formData();
   return api(request, {
-    uid: `${Date.now()}`,
     created_at: new Date(),
     text: data.get("text").toString(),
     done: false
